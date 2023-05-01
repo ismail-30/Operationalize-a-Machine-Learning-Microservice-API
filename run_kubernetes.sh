59d3edd087ba3d87d41ba1751e34c99fd6ce5814
ismail <ismail.arjdal2@gmail.com>
2023-05-01 02:36:03 +0100

    make updates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=ismail2019/ml_microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment ml-microservice --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/ml-microservice 8000:80 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="ismail2019/ml_microservice"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username ismail2019
+docker tag ml_microservice $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
